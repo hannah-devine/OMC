aeaafc613183ccba27f5be4dbc3dc2c4ba8e4884
hannah-devine <hannah96@hotmail.be>
2020-10-09 21:40:56 +0200

    adding text to localstorage works

--- tests/05-demo-localstorage/js/script.js
+++ tests/05-demo-localstorage/js/script.js
@@ -1,8 +1,23 @@
 {
 
+  const handleSubmitForm = e => {
+    e.preventDefault();
+    // de inhoud van de tekstvelden ophalen via het attribute value
+    const firstname = document.querySelector(`.firstname`).value;
+    console.log(firstname);
+    localStorage.setItem('name', firstname);
+  }
 
   const init = () => {
+    localStorage.setItem('myCat', 'Tom');
+    console.log(localStorage.key(0));
+    // hier haal ik van de key de value op
+    const value = localStorage.getItem('myCat');
+    console.log(value);
 
+    // luisteren naar het verzenden van het formulier
+    const $form = document.querySelector(`.form-register`);
+    $form.addEventListener(`submit`, handleSubmitForm);
   }
   init();
 }
