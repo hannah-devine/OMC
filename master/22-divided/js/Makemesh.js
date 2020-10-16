34524629a6d01e981f5190f3912086838bbae4ef
hannah-devine <hannah96@hotmail.be>
2020-10-16 10:57:43 +0200

    working prototype

---
+++ master/22-divided/js/Makemesh.js
@@ -0,0 +1,22 @@
+import { meshPosition } from "./MeshPosition.js";
+
+
+const makeMesh = (geometry, material, farDist) => {
+
+    const mesh = new THREE.Mesh(geometry, material, farDist);
+    const dist = farDist / 24;
+    const distDouble = dist * 2;
+    const tau = 2 * Math.PI; // One turn
+
+    meshPosition(mesh, dist, distDouble, tau);
+
+    // Manually control when 3D transformations recalculation occurs for better performance
+    mesh.matrixAutoUpdate = false;
+    mesh.updateMatrix();
+
+
+    return mesh;
+
+}
+
+export { makeMesh };
